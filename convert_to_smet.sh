e422b365c00a5bcf193f474fce79733acc5b3f5e
Nander Wever <nander.wever@slf.ch>
2024-03-18 15:49:12 +0100

    Correcting HS units and taking care of a basic filter to remove spikes.

--- convert_to_smet.sh
+++ convert_to_smet.sh
@@ -19,13 +19,16 @@ SiteSettings () {
 	esac
 }
 
-
 for site in ${sites}
 do
 	SiteSettings ${site}
 	echo "IMPORT_BEFORE = ./io_base.ini" > io.ini
 	echo "[Output]" >> io.ini
 	echo "METEOPATH = ./${site}" >> io.ini
+	echo "[FILTERS]" >> io.ini
+	echo "HS::filter1	= MAX" >> io.ini
+	echo "HS::arg1::soft	= FALSE" >> io.ini
+	echo "HS::arg1::max	= 258" >> io.ini
 	echo "[Input]" >> io.ini
 
 	let i=0
@@ -36,7 +39,7 @@ do
 		echo "POSITION${i}	= latlon ${lat} ${lon} ${alt}" >> io.ini
 		echo "CSV${i}_NAME	= ${sitename}" >> io.ini
 		echo "CSV${i}_ID	= ${siteid}" >> io.ini
-		echo "CSV${i}_FIELDS = " $(fgrep "Site Id" ${f} | awk -F, -f parse_fields.awk) >> io.ini
+		echo "CSV${i}_FIELDS	= " $(fgrep "Site Id" ${f} | awk -F, -f parse_fields.awk) >> io.ini
 	done
 
 	${pathtometeoiotimeseries}/meteoio_timeseries -c io.ini -b 2018-10-01 -e NOW
